4102304233af46bf884996229e4a50f06e77405e
hsuster <andrewhsu42@gmail.com>
2016-05-22 20:47:03 -0700

    Initial commit, I wish I knew what I was doing

---
+++ calibrationTest/calibrationTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calibrationTest
+{
+    public class CalibrationItem
+    {
+        internal string itemTitle { get; set; }
+        internal string itemTotal { get; set; }
+        internal string itemBase { get; set; }
+        internal string itemOffset { get; set; }
+        internal string itemPath { get; set; }
+    }
+
+    public class CalibrationGroup : CalibrationItem
+    {
+        public string groupTitle { get; set; }
+        internal List<CalibrationItem> myItems = new List<CalibrationItem>();
+    }
+
+    public class CalibrationModel
+    {
+        List<CalibrationGroup> myGroups = new List<CalibrationGroup>();
+    }
+}
---
+++ calibrationTest/calibrationTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calibrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalibrationGroup calibrationGroup1 = new CalibrationGroup();
+            calibrationGroup1.groupTitle = "group1";
+            calibrationGroup1.itemTitle = "myItemTitle1";
+            calibrationGroup1.itemTotal = "myItemTotal1";
+            calibrationGroup1.itemBase = "myItemBase1";
+
+            CalibrationGroup calibrationGroup2 = new CalibrationGroup();
+            calibrationGroup2.groupTitle = "group2";
+            calibrationGroup2.itemTitle = "myItemTitle2";
+            calibrationGroup2.itemTotal = "myItemTotal2";
+            calibrationGroup2.itemBase = "myItemBase2";
+
+            CalibrationGroup calibrationGroup3 = new CalibrationGroup();
+            calibrationGroup3.groupTitle = "group3";
+            calibrationGroup3.itemTitle = "myItemTitle3";
+            calibrationGroup3.itemTotal = "myItemTotal3";
+            calibrationGroup3.itemBase = "myItemBase3";
+
+            Console.WriteLine(calibrationGroup1.groupTitle);
+            Console.WriteLine(calibrationGroup1.itemBase);
+            Console.WriteLine(calibrationGroup2.groupTitle);
+            Console.WriteLine(calibrationGroup2.itemBase);
+            Console.WriteLine(calibrationGroup3.groupTitle);
+            Console.WriteLine(calibrationGroup3.itemBase);
+
+        }
+    }
+}
